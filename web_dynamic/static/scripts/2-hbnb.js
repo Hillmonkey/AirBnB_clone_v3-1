0091563e13d6d4bb26c5815ebacc5a7510d13d62
wendysegura <118@holbertonschool.com>
2018-01-17 22:22:10 +0000

    upload #3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,34 @@
+let amens = {};
+$(function () {
+  $('input[type=checkbox]').click(function () {
+    console.log('click');
+    if (this.checked) {
+      amens[this.dataset.id] = this.dataset.name;
+    } else {
+      delete amens[this.dataset.id];
+    }
+    if ($.isEmptyObject(amens)) {
+      $('div.amenities > h4').html('&nbsp;');
+    } else {
+      let amenStr = Object.values(amens).toString();
+      $('div.amenities > h4').html(amenStr);
+    }
+  });
+});
+$.ajax({
+    // The URL for the request
+    url: "http://0.0.0.0:5051/api/v1/status/",
+        // The data to send (will be converted to a query string)
+    // Whether this is a POST or GET request
+    type: "GET",
+ 
+    // The type of data we expect back
+    dataType : "json"
+})
+  // Code to run if the request succeeds (is done);
+  // The response is passed to the function
+  .done(function(data) {
+     if (data.status == 'OK') {
+       $('#api_status').addClass('available');
+     } 
+  });

212a9486859d437c26ab267cb33478f216bb8a01
Larry Madeo <lmadeo76@gmail.com>
2018-01-18 05:28:58 +0000

    first iteration

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -17,21 +17,38 @@ $(function () {
 });
 
 $.ajax({
-    // The URL for the request
-    url: "http://0.0.0.0:5051/api/v1/status/",
-        // The data to send (will be converted to a query string)
-    // Whether this is a POST or GET request
-    type: "GET",
-    // The type of data we expect back
-    dataType : "json"
+  // The URL for the request
+  url: 'http://0.0.0.0:5051/api/v1/status/',
+  // The data to send (will be converted to a query string)
+  // Whether this is a POST or GET request
+  type: 'GET',
+
+  // The type of data we expect back
+  dataType: 'json'
 })
   // Code to run if the request succeeds (is done);
   // The response is passed to the function
-  .done(function(data) {
-     if (data.status == 'OK') {
-       $('#api_status').addClass('available');
-     } 
+  .done(function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    }
   });
 
-// POST /api/v1/places_sea
+// POST /api/v1/places_search
+let placeUrl = 'http://0.0.0.0:5001/api/vi/places_search/';
+$.post(placeUrl).done(function (data) {
+  $('.result').html(data);
+  console.log('Load was performed.');
+});
 
+/*
+$.ajax({
+  url: 'http://0.0.0.0:5051/api/v1/places_search/',
+  type: 'POST',
+  dataType: 'json'
+})
+.done(function (data) {
+  console.log('place search');
+  console.log(data);
+});
+*/

b71b0b3ab84ba8f04656aed440c2598c734e7d97
Larry Madeo <lmadeo76@gmail.com>
2018-01-19 07:24:50 +0000

    #5 now functioning

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,4 +1,7 @@
 let amens = {};
+let amensPlaceUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
+
+// ============================================================================
 $(function () {
   $('input[type=checkbox]').click(function () {
     if (this.checked) {
@@ -14,34 +17,71 @@ $(function () {
     }
   });
 });
-//============================================================================
+
+// ===========================================================================
 
 $.ajax({
-  // The URL for the request
   url: 'http://0.0.0.0:5001/api/v1/status/',
-  // The data to send (will be converted to a query string)
-  // Whether this is a POST or GET request
   type: 'GET',
-
-  // The type of data we expect back
   dataType: 'json'
 })
-  // Code to run if the request succeeds (is done);
-  // The response is passed to the function
   .done(function (data) {
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
     }
   });
-//============================================================================
 
-$('button#search').click(function () {
-  console.log("WHAT???");
-}
-//============================================================================
+// ===========================================================================
+
+$(function () {
+  $('button#search').click(function () {
+    let AmensIDs = Object.keys(amens);
+    AmensIDs = JSON.stringify({'amenities': AmensIDs});
+    console.log(AmensIDs);
+    $.ajax({
+      url: amensPlaceUrl,
+      type: 'POST',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: AmensIDs
+    })
+      .done(function (data) {
+        $('SECTION.places').children().remove();
+        data.forEach(function (place) {
+          $('SECTION.places').append(
+            '<article>' +
+             '<h2>' + place.name + '</h2>' +
+             '<div class="price_by_night">' +
+               '<p>$' + place.price_by_night + '</p>' +
+             '</div>' +
+             '<div class="information">' +
+               '<div class="max_guest">' +
+                 '<div class="guest_image"></div>' +
+                   '<p>' + place.max_guest + '</p>' +
+                 '</div>' +
+               '<div class="number_rooms">' +
+                 '<div class="bed_image"></div>' +
+                 '<p>' + place.number_rooms + '</p>' +
+               '</div>' +
+               '<div class="number_bathrooms">' +
+                 '<div class="bath_image"></div>' +
+                 '<p>' + place.number_bathrooms + '</p>' +
+               '</div>' +
+             '</div>' +
+             '<div class="description">' +
+               '<p>' + place.description + '</p>' +
+             '</div>' +
+           '</article>');
+        });
+      });
+  });
+});
+
+// ===========================================================================
+
 $.ajax({
   // The URL for the request
-  url: 'http://0.0.0.0:5001/api/v1/places_search/',
+  url: amensPlaceUrl,
   // The data to send (will be converted to a query string)
   // Whether this is a POST or GET request
   type: 'POST',
